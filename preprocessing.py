58f0532608fab53001be045ffa85223df18eeaa5
Kinnera323 <skinnera323@gmail.com>
2017-02-23 09:57:32 +0530

    Inverted index

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,13 @@ from nltk import word_tokenize
 from nltk.corpus import reuters 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.porter import PorterStemmer
+from collections import defaultdict
 import re
 from nltk.corpus import stopwords
 import csv
 
+all_words_train = {}
+all_words_test = {}
 cachedStopWords = stopwords.words("english")
 
 def tokenize(text):
@@ -54,31 +57,81 @@ def collection_stats():
 	# Raw document
 	print(reuters.raw(document_id));
 
+
+
+# Initial functions which gets the required data for indexing
+def build_index_test(doc_data,doc_id):
+	for i in doc_data:
+		if i in all_words_test:
+			if doc_id in all_words_test[i]:
+				continue
+			else:
+				x = all_words_test[i]
+				all_words_test[i] += doc_id
+				all_words_test[i] += ' '				
+		else:
+			all_words_test[i] = defaultdict(list)
+			all_words_test[i] = doc_id
+			all_words_test[i] += ' '	
+
+def build_index_train(doc_data,doc_id):
+	for i in doc_data:
+		if i in all_words_train:
+			if doc_id in all_words_train[i]:
+				continue
+			else:
+				x = all_words_train[i]
+				all_words_train[i] += doc_id
+				all_words_train[i] += ' '				
+		else:
+			all_words_train[i] = defaultdict(list)
+			all_words_train[i] = doc_id
+			all_words_train[i] += ' '	
+		
+
 def main():
 	train_docs = []
 	test_docs = []
 	order_test_docs = []
 	test_list_data = []
-	with open("output.csv","wb") as f:
+	with open("Frequent_train.csv","wb") as f:
 		writer = csv.writer(f,quoting=csv.QUOTE_ALL)	
 		for doc_id in reuters.fileids():
-			if doc_id.startswith("train"):		
-				train_docs.append(reuters.raw(doc_id))
-			else:
+			if doc_id.startswith("train"):
 				writer.writerow(tokenize(reuters.raw(doc_id)))
-				order_test_docs.append(doc_id)
+				#order_train_docs.append(doc_id)
+				print "######"		
+				train_docs.append(reuters.raw(doc_id))
+				doc_number = doc_id.split('/')[1]
+				build_index_train(tokenize(reuters.raw(doc_id)),doc_number)
+				
+			else:
+				#writer.writerow(tokenize(reuters.raw(doc_id)))
+				#order_test_docs.append(doc_id)
 				#test_list_data.append(tokenize(reuters.raw(doc_id)))
-				print "######"
+				#print "######"
+				#doc_number = doc_id.split('/')[1]
+				#build_index_test(tokenize(reuters.raw(doc_id)),doc_number)
 				test_docs.append(reuters.raw(doc_id))
 	
 	
-		
-		
-		
-	exit()	
-	representer = tf_idf(train_docs);
+		#print(all_words)
+	exit()
 
-	for doc in test_docs:
-		print(feature_values(doc, representer))
+	inverted_file_test = {}
+	for i in all_words_test:
+		e = all_words_test[i].split(' ')[:-1]
+		if len(e)>= 3 and len(e) <= 90:
+			inverted_file_test[i] = e
+
+	inverted_file_train = {}
+	for i in all_words_train:
+		e = all_words_train[i].split(' ')[:-1]
+		if len(e)>= 3 and len(e) <= 90:
+			inverted_file_train[i] = e
+			
+	print(inverted_file_train)	
+	exit()	
+
 
 main()
